59ae47f1db358d2f5fd699eaccfc1de6084cc0e7
Mahdi Ahmed <mahdiahmed1416@gmail.com>
2024-07-12 23:25:38 -0300

    Added log statement

--- src/main/java/com/example/service1/MainController.java
+++ src/main/java/com/example/service1/MainController.java
@@ -53,7 +53,7 @@ public class MainController {
 
     @PostMapping("/calculate")
     public ResponseEntity<Output> calculate(@RequestBody Input input) {
-        log.info("In calculate api..");
+        log.info("In calculate api....");
         validateInput(input);
 
         return ResponseEntity.ok(getOutputFromService2(input));
